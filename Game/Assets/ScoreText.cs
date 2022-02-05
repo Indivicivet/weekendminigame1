3ce9787803162a5d75e7eb12987682ccedc10f49
Indivicivet <indivicivet@gmail.com>
2022-02-05 00:53:44 +0000

    rescale score text depending on score

--- Game/Assets/ScoreText.cs
+++ Game/Assets/ScoreText.cs
@@ -8,16 +8,23 @@ public class ScoreText : MonoBehaviour
 {
     private GameState gameState;
 
+    private Vector3 referenceScale;
+
     // Start is called before the first frame update
     void Start()
     {
         // probably not the way to do this
         gameState = GameObject.Find("GameStateHolder").GetComponent<GameState>();
+
+        referenceScale = GetComponent<Transform>().localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<TextMeshPro>().SetText("Score: " + gameState.score.ToString("F2"));
+
+        var rescale = 0.5f + 0.8f * Mathf.Max(Mathf.Atan(gameState.score * 0.2f) / (Mathf.PI * 0.5f), 0.0f);
+        GetComponent<Transform>().localScale = referenceScale * rescale;
     }
 }
