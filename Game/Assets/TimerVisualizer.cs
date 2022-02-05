4abeb74509235f8b6a815e9e50e52aec818db37a
Indivicivet <indivicivet@gmail.com>
2022-02-05 00:48:02 +0000

    more random timers for fun and make them scale properly

--- Game/Assets/TimerVisualizer.cs
+++ Game/Assets/TimerVisualizer.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class TimerVisualizer : MonoBehaviour
 {
-    private float maxScale = 1.5f;
+    private float originalYScale;
 
     private GameState gameState;
 
     // Start is called before the first frame update
     void Start()
     {
+        originalYScale = GetComponent<Transform>().localScale.y;
         // todo :: not the best way to do this? ^^'
         gameState = GameObject.Find("GameStateHolder").GetComponent<GameState>();
     }
@@ -19,7 +20,12 @@ public class TimerVisualizer : MonoBehaviour
     void Update()
     {
         if (gameState.timeLeft > 0) {
-            GetComponent<Transform>().localScale = new Vector3(1, maxScale * gameState.timeLeft / gameState.totalTime, 1);
+            var tf = GetComponent<Transform>();
+            tf.localScale = new Vector3(
+                tf.localScale.x,
+                originalYScale * gameState.timeLeft / gameState.totalTime,
+                tf.localScale.z
+            );
         } else {
             GetComponent<MeshRenderer>().enabled = false;
         }
